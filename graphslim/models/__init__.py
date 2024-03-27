0ecd150740947c1c6fb8177530bb9c7f08c571d0
juntongni <juntongni02@gmail.com>
2024-03-27 12:46:10 +0800

    Change __init__.py to import all models

--- graphslim/models/__init__.py
+++ graphslim/models/__init__.py
@@ -1,1 +1,8 @@
 from .gcn import GCN
+from .sgc import SGC
+from .sgc_multi import SGC as SGC1
+from .myappnp import APPNP
+from .myappnp1 import APPNP1
+from .mycheby import Cheby
+from .mygraphsage import GraphSage
+from .gat import GAT
