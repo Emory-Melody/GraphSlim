d7ee0c4bdbdd4a0bb5273c0b0421aa3dd89de0d9
juntongni <juntongni02@gmail.com>
2024-04-19 10:44:49 +0800

    run 3 times for coarse methods

--- graphslim/train_coarsen.py
+++ graphslim/train_coarsen.py
@@ -1,19 +1,24 @@
+import numpy as np
+
 from configs import *
 from evaluation.eval_agent import Evaluator
 from graphslim.coarsening.coarsening_agent import Coarsen
 from graphslim.dataset import *
 
-args = cli(standalone_mode=False)
-
-graph = get_dataset(args.dataset, args)
-agent = Coarsen(setting=args.setting, data=graph, args=args)
-reduced_graph = agent.reduce(graph)
-
-# print("num of synthetic node", reduced_graph.feat_syn.shape[0])
-if args.setting == 'trans':
-    print("real reduction rate", reduced_graph.feat_syn.shape[0] / graph.x.shape[0] * 100, "%")
-else:
-    print("real reduction rate", reduced_graph.feat_syn.shape[0] / sum(graph.train_mask) * 100, "%")
-
-evaluator = Evaluator(args)
-evaluator.evaluate(reduced_graph, 'GCN')
+if __name__ == '__main__':
+    args = cli(standalone_mode=False)
+    graph = get_dataset(args.dataset, args)
+    all_res = []
+    for i in range(args.run_reduction):
+        args.seed += i
+        agent = Coarsen(setting=args.setting, data=graph, args=args)
+        reduced_graph = agent.reduce(graph)
+        if args.setting == 'trans':
+            print("real reduction rate", reduced_graph.feat_syn.shape[0] / graph.x.shape[0] * 100, "%")
+        else:
+            print("real reduction rate", reduced_graph.feat_syn.shape[0] / sum(graph.train_mask) * 100, "%")
+        evaluator = Evaluator(args)
+        res = evaluator.evaluate(reduced_graph, 'GCN')
+        all_res.append(res)
+    all_res = np.array(all_res).reshape(-1)
+    print('Final Mean:', all_res.mean(), '+/-', all_res.std())
