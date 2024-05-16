6ea8f3f5af4f71237a7e391d09b1a042e24cf304
rockcorj <jshmhsb@gmail.com>
2024-05-16 15:46:00 +0800

    interval=epochs//10

--- graphslim/configs.py
+++ graphslim/configs.py
@@ -35,6 +35,7 @@ def setting_config(args):
     args.pre_norm = True
     args.hidden = 256
     args.run_inter_eval = 3
+    args.eval_interval = args.epochs // 10
     args.checkpoints = range(-1, args.epochs + 1, args.eval_interval)
     args.eval_epochs = 300
     args.eval_model = 'GCN'
