e6c03445af985ec14d3e2dba48d279398f9e3604
rockcorj <jshmhsb@gmail.com>
2024-05-21 15:57:12 +0800

    add attack PRBCD

--- graphslim/condense_with_attack.py
+++ graphslim/condense_with_attack.py
@@ -12,26 +12,49 @@ from graphslim.dataset import *
 import logging
 from deeprobust.graph.defense import GCN
 from deeprobust.graph.global_attack import DICE, random_attack, Metattack, PRBCD
+import scipy.sparse as sp
 import time
 
 args = cli(standalone_mode=False)
 args.ptb_r = 0.25
 
 data = get_dataset(args.dataset, args)
-if args.attack == 'random_attack':
-    pass
+if not os.path.exists(args.save_path + '/corrupt_graph'):
+    os.makedirs(args.save_path + '/corrupt_graph')
+args.save_path = f'{args.save_path}/corrupt_graph'
 
-if args.attack == 'metattack':
-    adj, features, labels = data.adj_full, data.feat_full, data.labels_full
-    args.ptb_n = int(args.ptb_r * (adj.sum() // 2))
+if os.path.exists(f'{args.save_path}/adj_{args.dataset}_{args.attack}_{args.ptb_r}.npz'):
     if args.setting == 'ind':
-        model = PRBCD(data, device=args.device)
-        data.edge_index, _ = model.attack(csr2ei(data.adj_train), ptb_rate=args.ptb_r)
-        data.adj_train = ei2csr(data.edge_index.cpu(), data.num_nodes)
+        data.adj_train = sp.load_npz(f'{args.save_path}/adj_{args.dataset}_{args.attack}_{args.ptb_r}.npz')
     else:
-        model = PRBCD(data, device=args.device)
-        data.edge_index, _ = model.attack(data.edge_index, ptb_rate=args.ptb_r)
-        data.adj_full = ei2csr(data.edge_index.cpu(), data.num_nodes)
+        data.adj_full = sp.load_npz(f'{args.save_path}/adj_{args.dataset}_{args.attack}_{args.ptb_r}.npz')
+
+else:
+    if args.attack == 'metattack':
+        if args.setting == 'ind':
+            adj = data.adj_train
+            args.ptb_n = int(args.ptb_r * (adj.sum() // 2))
+            model = PRBCD(data, device=args.device)
+            edge_index, _ = model.attack(ptb_rate=args.ptb_r)
+            data.adj_train = ei2csr(edge_index.cpu(), data.num_nodes)[np.ix_(data.idx_train, data.idx_train)]
+            sp.save_npz(f'{args.save_path}/adj_{args.dataset}_{args.attack}_{args.ptb_r}.npz', data.adj_train)
+            print(f'save corrupt graph at adj_{args.dataset}_{args.attack}_{args.ptb_r}.npz')
+        else:
+            adj, features, labels = data.adj_full, data.feat_full, data.labels_full
+            args.ptb_n = int(args.ptb_r * (adj.sum() // 2))
+            model = PRBCD(data, device=args.device)
+            data.edge_index, _ = model.attack(data.edge_index, ptb_rate=args.ptb_r)
+            data.adj_full = ei2csr(data.edge_index.cpu(), data.num_nodes)
+            sp.save_npz(f'{args.save_path}/adj_{args.dataset}_{args.attack}_{args.ptb_r}.npz', data.adj_full)
+            print(f'save corrupt graph at {args.save_path}/adj_{args.dataset}_{args.attack}_{args.ptb_r}.npz')
+    if args.attack == 'random_attack':
+        if args.setting == 'ind':
+            model = random_attack()
+            data.adj_train = model.attack(data.adj_train, args.ptb_r)
+            np.save(f'{args.save_path}/adj_{args.dataset}_{args.attack}_{args.ptb_r}.npy', data.adj_train)
+        else:
+            data.adj_full = random_attack(data.adj_full, args.ptb_r)
+            np.save(f'{args.save_path}/adj_{args.dataset}_{args.attack}_{args.ptb_r}.npy', data.adj_full)
 if args.method == 'gcond':
     agent = GCond(setting=args.setting, data=data, args=args)
 elif args.method == 'doscond':
