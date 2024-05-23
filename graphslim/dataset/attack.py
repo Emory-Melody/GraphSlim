3923bbfbaf30cbe2fb67894a857eeedabd1d8f52
rockcorj <jshmhsb@gmail.com>
2024-05-23 19:08:16 +0800

    specify buffer for attack specify code for random_feature

--- graphslim/dataset/attack.py
+++ graphslim/dataset/attack.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import torch
 
 import numpy as np
 
@@ -14,43 +15,57 @@ import scipy.sparse as sp
 
 def attack(data, args):
     args.save_path = f'{args.save_path}/corrupt_graph/{args.attack}'
+    gcn_model = GCN(nfeat=data.x.shape[1], nhid=args.hidden, nclass=data.nclass, args=args, mode='attack').to(
+        args.device)
     if not os.path.exists(args.save_path):
         os.makedirs(args.save_path)
 
-    if os.path.exists(f'{args.save_path}/adj_{args.dataset}_{args.attack}_{args.ptb_r}.npz'):
-        if args.setting == 'ind':
-            data.adj_train = sp.load_npz(f'{args.save_path}/adj_{args.dataset}_{args.attack}_{args.ptb_r}.npz')
-        else:
-            data.adj_full = sp.load_npz(f'{args.save_path}/adj_{args.dataset}_{args.attack}_{args.ptb_r}.npz')
+    if args.attack in ['metattack', 'random']:
+        if os.path.exists(f'{args.save_path}/adj_{args.dataset}_{args.attack}_{args.ptb_r}.npz'):
+            if args.setting == 'ind':
+                data.adj_train = sp.load_npz(f'{args.save_path}/adj_{args.dataset}_{args.attack}_{args.ptb_r}.npz')
+            else:
+                data.adj_full = sp.load_npz(f'{args.save_path}/adj_{args.dataset}_{args.attack}_{args.ptb_r}.npz')
+            print(f'load corrupt graph at {args.save_path}/adj_{args.dataset}_{args.attack}_{args.ptb_r}.npz')
 
-    else:
-        gcn_model = GCN(nfeat=data.x.shape[1], nhid=args.hidden, nclass=data.nclass, args=args, mode='attack').to(
-            args.device)
-        if args.setting == 'ind':
-            adj = data.adj_train
-            args.ptb_n = int(args.ptb_r * (adj.sum() // 2))
         else:
-            adj = data.adj_full
-            args.ptb_n = int(args.ptb_r * (adj.sum() // 2))
-        if args.attack == 'metattack':
             if args.setting == 'ind':
-                model = PRBCD(data, device=args.device)
-                # ignore the test results!
-                edge_index, _ = model.attack(ptb_rate=args.ptb_r)
-                data.adj_train = ei2csr(edge_index.cpu(), data.num_nodes)[np.ix_(data.idx_train, data.idx_train)]
+                adj = data.adj_train
+                args.ptb_n = int(args.ptb_r * (adj.sum() // 2))
             else:
-                model = PRBCD(data, device=args.device)
-                data.edge_index, _ = model.attack(data.edge_index, ptb_rate=args.ptb_r)
-                data.adj_full = ei2csr(data.edge_index.cpu(), data.num_nodes)
-        if args.attack == 'random':
-            model = RandomAttack()
+                adj = data.adj_full
+                args.ptb_n = int(args.ptb_r * (adj.sum() // 2))
+            if args.attack == 'metattack':
+                if args.setting == 'ind':
+                    model = PRBCD(data, device=args.device)
+                    # ignore the test results!
+                    edge_index, _ = model.attack(ptb_rate=args.ptb_r)
+                    data.adj_train = ei2csr(edge_index.cpu(), data.num_nodes)[np.ix_(data.idx_train, data.idx_train)]
+                else:
+                    model = PRBCD(data, device=args.device)
+                    data.edge_index, _ = model.attack(data.edge_index, ptb_rate=args.ptb_r)
+                    data.adj_full = ei2csr(data.edge_index.cpu(), data.num_nodes)
+            elif args.attack == 'random':
+                model = RandomAttack()
+                if args.setting == 'ind':
+                    model.attack(data.adj_train, n_perturbations=args.ptb_n, type='add')
+                    data.adj_train = model.modified_adj.tocsr()
+                else:
+                    model.attack(data.adj_full, n_perturbations=args.ptb_n, type='add')
+                    data.adj_full = model.modified_adj.tocsr()
+            gcn_model.fit_with_val(data, train_iters=args.eval_epochs, verbose=args.verbose, setting=args.setting)
+            test_acc = gcn_model.test(data, setting=args.setting, verbose=True)
+            args.logger.info(f'attack {args.attack}_{args.ptb_r} test acc: {test_acc}')
+            sp.save_npz(f'{args.save_path}/adj_{args.dataset}_{args.attack}_{args.ptb_r}.npz', data.adj_train)
+            print(f'save corrupt graph at {args.save_path}/adj_{args.dataset}_{args.attack}_{args.ptb_r}.npz')
+    elif args.attack == 'random_feat':
+        if os.path.exists(f'{args.save_path}/feat_{args.dataset}_{args.attack}_{args.ptb_r}.pt'):
             if args.setting == 'ind':
-                model.attack(data.adj_train, n_perturbations=args.ptb_n, type='add')
-                data.adj_train = model.modified_adj.tocsr()
+                data.feat_train = torch.load(f'{args.save_path}/feat_{args.dataset}_{args.attack}_{args.ptb_r}.pt')
             else:
-                model.attack(data.adj_full, n_perturbations=args.ptb_n, type='add')
-                data.adj_full = model.modified_adj.tocsr()
-        if args.attack == 'random_feat':
+                data.feat_full = torch.load(f'{args.save_path}/feat_{args.dataset}_{args.attack}_{args.ptb_r}.pt')
+            print(f'load corrupt graph at {args.save_path}/feat_{args.dataset}_{args.attack}_{args.ptb_r}.pt')
+        else:
             model = RandomAttack(attack_structure=False, attack_features=True)
             args.ptb_n = int(args.ptb_r * data.x.shape[1])
             if args.setting == 'ind':
@@ -59,9 +74,10 @@ def attack(data, args):
             else:
                 model.attack(data.feat_full, n_perturbations=args.ptb_n)
                 data.feat_full = model.modified_features
-        gcn_model.fit_with_val(data, train_iters=args.eval_epochs, verbose=args.verbose, setting=args.setting)
-        test_acc = gcn_model.test(data, setting=args.setting, verbose=True)
-        args.logger.info(f'attack {args.attack}_{args.ptb_r} test acc: {test_acc}')
-        sp.save_npz(f'{args.save_path}/adj_{args.dataset}_{args.attack}_{args.ptb_r}.npz', data.adj_train)
-        print(f'save corrupt graph at adj_{args.dataset}_{args.attack}_{args.ptb_r}.npz')
+            gcn_model.fit_with_val(data, train_iters=args.eval_epochs, verbose=args.verbose, setting=args.setting)
+            test_acc = gcn_model.test(data, setting=args.setting, verbose=True)
+            args.logger.info(f'attack {args.attack}_{args.ptb_r} test acc: {test_acc}')
+            sp.save_npz(f'{args.save_path}/feat_{args.dataset}_{args.attack}_{args.ptb_r}.pt', data.adj_train)
+            print(f'save corrupt graph at {args.save_path}/feat_{args.dataset}_{args.attack}_{args.ptb_r}.pt')
+
     return data
