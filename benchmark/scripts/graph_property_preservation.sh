e85023013baabfa9ed20f2733521162bd0374b14
Rock <59243884+rockcor@users.noreply.github.com>
2024-06-12 12:21:08 +0800

    更新 graph_property_preservation.sh

--- benchmark/scripts/graph_property_preservation.sh
+++ benchmark/scripts/graph_property_preservation.sh
@@ -1,12 +1,10 @@
 # Graph Property Preservation
 # Must obtain condensed graph of each methods by running performance.sh before running this script.
 for dataset in cora citeseer ogbn-arxiv flickr reddit; do
-    python ../graph_property.py -D $dataset
     python ../graph_property.py -D $dataset -W
 done
 
 
 for dataset in cora citeseer ogbn-arxiv flickr reddit; do
-    python ../graph_property_no_structure.py -D $dataset
     python ../graph_property_no_structure.py -D $dataset -W
 done
